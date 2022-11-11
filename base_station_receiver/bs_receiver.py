7e8631bd4916a1daa82846d2b635fff63f80aa7f
Jacob Moore <jlmoors001@gmail.com>
2022-11-11 01:14:46 -0600

    Initial commit

---
+++ base_station_receiver/bs_receiver.py
@@ -0,0 +1,3 @@
+"""
+Justin TODO: Python script that receives messages from the base station repeatedly, determines if they are waypoints, and if they are, immediately publish them to "/waypoints" topic (list of [lat, long] points). If they are not waypoints, send the correction data to the sparkfun. Study ublox docs to do this. You should be able to handle all of this in this file. 
+"""
