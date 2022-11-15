30fe97a855a2bb8aae5ee0d3bdd9b1076fc7a57c
jusan1 <55926943+jusan1@users.noreply.github.com>
2022-11-14 19:05:46 -0600

    Temp Update

--- scripts/send_waypoints.py
+++ scripts/send_waypoints.py
@@ -3,14 +3,29 @@ User interface for sending messages to UGV
 """
 from pymavlink import mavutil
 import time
+import os
 
 master = mavutil.mavlink_connection("/dev/ttyUSB0", baud=57600)
 
 while True:
     cmd = input("Enter UGV command >  ")
-
+    
+    if cmd == "waypoints":
+       file_path  = input("Type in a file path")
+        if(os.path.exists(file_path)):
+            file = open(file_path,"read")
+ 
     if cmd == "start":
         None
+        
+    if cmd == "clear":
+       
+        None
+    if cmd == "stop"
+        None
+    
+    if cmd == ""
+    
     if cmd == "help":
         print("\nwaypoints: prompts waypoint file input")
         print("start: starts navigation to waypoints")
